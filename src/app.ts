2b2582c4e0f5937d293c6f6ba59b3d5c511bf169
PhucAn04 <pham27282@gmail.com>
2025-11-13 23:12:54 +0700

    add view engine

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,12 @@ const app = express();
 const port = 8080;
 const PORT = process.env.PORT || 8080;
 
+// config view engine
+app.set("view engine", "ejs");
+app.set("views", "./src/views");
+
 app.get("/", (req, res) => {
-  res.send("Hello An!");
+  res.render("home");
 });
 
 app.listen(port, () => {
